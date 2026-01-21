b2e67040d22c953a718f77fbd8946d391fedcdbd
Max <38135396+3v3r51nc3@users.noreply.github.com>
2026-01-22 00:21:08 +0100

    Initial commit

---
+++ src/tpHistogram.cpp
@@ -0,0 +1,172 @@
+#include "tpHistogram.h"
+#include <cmath>
+#include <algorithm>
+#include <tuple>
+using namespace cv;
+using namespace std;
+
+std::string type2str(int type)
+{
+    std::string r;
+
+    uchar depth = type & CV_MAT_DEPTH_MASK;
+    uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+    switch (depth)
+    {
+    case CV_8U:
+        r = "CV_8U";
+        break;
+    case CV_8S:
+        r = "CV_8S";
+        break;
+    case CV_16U:
+        r = "CV_16U";
+        break;
+    case CV_16S:
+        r = "CV_16S";
+        break;
+    case CV_32S:
+        r = "CV_32S";
+        break;
+    case CV_32F:
+        r = "CV_32F";
+        break;
+    case CV_64F:
+        r = "CV_64F";
+        break;
+    default:
+        r = "Unknown";
+        break;
+    }
+
+    r += "C";
+    r += std::to_string(chans);
+
+    return r;
+}
+
+/**
+    Inverse a grayscale image with float values.
+    for all pixel p: res(p) = 1.0 - image(p)
+*/
+Mat inverse(Mat image)
+{
+    // clone original image
+    Mat res = image.clone();
+    /********************************************
+              YOUR CODE HERE
+    *********************************************/
+
+
+    /********************************************
+                END OF YOUR CODE
+    *********************************************/
+    return res;
+}
+
+/**
+    Thresholds a grayscale image with float values.
+    for all pixel p: res(p) =
+        | 0 if image(p) <= lowT
+        | image(p) if lowT < image(p) <= hightT
+        | 1 otherwise
+*/
+Mat threshold(Mat image, float lowT, float highT)
+{
+    Mat res = image.clone();
+    assert(lowT <= highT);
+    /********************************************
+                YOUR CODE HERE
+    *********************************************/
+
+    /********************************************
+                END OF YOUR CODE
+    *********************************************/
+    return res;
+}
+
+/**
+    Quantize the input float image in [0,1] in numberOfLevels different gray levels.
+
+    eg. for numberOfLevels = 3 the result should be for all pixel p: res(p) =
+        | 0 if image(p) < 1/3
+        | 1/2 if 1/3 <= image(p) < 2/3
+        | 1 otherwise
+
+        for numberOfLevels = 4 the result should be for all pixel p: res(p) =
+        | 0 if image(p) < 1/4
+        | 1/3 if 1/4 <= image(p) < 1/2
+        | 2/3 if 1/2 <= image(p) < 3/4
+        | 1 otherwise
+
+        and so on for other values of numberOfLevels.
+
+*/
+Mat quantize(Mat image, int numberOfLevels)
+{
+    Mat res = image.clone();
+    assert(numberOfLevels > 0);
+    /********************************************
+                YOUR CODE HERE
+    *********************************************/
+
+    /********************************************
+                END OF YOUR CODE
+    *********************************************/
+    return res;
+}
+
+/**
+    Normalize a grayscale image with float values
+    Target range is [minValue, maxValue].
+*/
+Mat normalize(Mat image, float minValue, float maxValue)
+{
+    Mat res = image.clone();
+    assert(minValue <= maxValue);
+    /********************************************
+                YOUR CODE HERE
+    *********************************************/
+
+    /********************************************
+                END OF YOUR CODE
+    *********************************************/
+    return res;
+}
+
+/**
+    Equalize image histogram with unsigned char values ([0;255])
+
+    Warning: this time, image values are unsigned chars but calculation will be done in float or double format.
+    The final result must be rounded toward the nearest integer
+*/
+Mat equalize(Mat image)
+{
+    Mat res = image.clone();
+    /********************************************
+                YOUR CODE HERE
+    *********************************************/
+
+    /********************************************
+                END OF YOUR CODE
+    *********************************************/
+    return res;
+}
+
+/**
+    Compute a binarization of the input image using an automatic Otsu threshold.
+    Input image is of type unsigned char ([0;255])
+*/
+Mat thresholdOtsu(Mat image)
+{
+    Mat res = image.clone();
+    /********************************************
+                YOUR CODE HERE
+    *********************************************/
+
+    /********************************************
+                END OF YOUR CODE
+    *********************************************/
+    return res;
+}
